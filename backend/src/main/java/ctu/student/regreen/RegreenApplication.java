18c591963a007ca9e51aa1cd9f55d66ef30261b1
nhutit05 <nhut.vlg2005@gmail.com>
2026-05-27 08:01:26 +0700

    feat: init settings

---
+++ backend/src/main/java/ctu/student/regreen/RegreenApplication.java
@@ -0,0 +1,13 @@
+package ctu.student.regreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegreenApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ctu/student/regreen/RegreenApplicationTests.java
@@ -0,0 +1,13 @@
+package ctu.student.regreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
