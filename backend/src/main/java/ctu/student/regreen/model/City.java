e668528307a703f48c186adae92ee25e898389c1
nhutvdev <nhut.vlg2005@gmail.com>
2026-06-07 11:39:30 +0000

    feat: refactor some created tables

--- backend/src/main/java/ctu/student/regreen/model/City.java
+++ backend/src/main/java/ctu/student/regreen/model/City.java
@@ -1,66 +1,43 @@
 package ctu.student.regreen.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Table(name = "cities")
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "villages")
 public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer city_id;
+
+    @NotBlank(message = "Tên tỉnh/thành phố không được trống")
+    @Column(nullable = false)
     private String city_name;
-    private String city_code;
 
-    public City() {
-        this.city_id = -1;
-        this.city_name = "";
-        this.city_code = "0";
-    }
+    @NotBlank
+    @Column(nullable = false)
+    private String city_level;
 
-    public City(Integer city_id, String city_name, String city_code) {
-        this.city_id = city_id;
-        this.city_name = city_name;
-        this.city_code = city_code;
-    }
-
-    public Integer getCity_id() {
-        return city_id;
-    }
-
-    public void setCity_id(Integer city_id) {
-        this.city_id = city_id;
-    }
-
-    public String getCity_name() {
-        return city_name;
-    }
-
-    public void setCity_name(String city_name) {
-        this.city_name = city_name;
-    }
-
-    public String getCity_code() {
-        return city_code;
-    }
-
-    public void setCity_code(String city_code) {
-        this.city_code = city_code;
-    }
-
-    @Override
-    public String toString() {
-        return "City{" +
-                "city_id=" + city_id +
-                ", city_name='" + city_name + '\'' +
-                ", city_code=" + city_code +
-                '}';
-    }
+    @OneToMany(mappedBy = "city", fetch = FetchType.LAZY)
+    private List<Village> villages = new ArrayList<>();
 }
