c244396035c2a2c3c025c6456732b49088556571
nhutvdev <nhut.vlg2005@gmail.com>
2026-06-08 08:56:09 +0000

    feat: add some tables for admin features

--- backend/src/main/java/ctu/student/regreen/model/Material.java
+++ backend/src/main/java/ctu/student/regreen/model/Material.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
