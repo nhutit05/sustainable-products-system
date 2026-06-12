91743dc2b731b334a785cd3c5804b87f727deba0
nhutvdev <nhut.vlg2005@gmail.com>
2026-06-12 13:27:12 +0000

    refactor: remove static function and replace with component annotation for implement service

--- backend/src/main/java/ctu/student/regreen/model/Voucher.java
+++ backend/src/main/java/ctu/student/regreen/model/Voucher.java
@@ -35,22 +35,15 @@ public class Voucher {
     private Integer voucherId;
 
     @Column(nullable = false, updatable = false, unique = true, length = 50, name = "code")
-    @NotBlank
-    @Size(max = 50)
     private String code;
 
     @Column(nullable = false, columnDefinition = "TEXT", name = "description")
-    @NotBlank
     private String description;
 
-    @Positive
-    @NotNull
     @Column(nullable = false, name = "discount_value")
     private Float discountValue;
 
-    @NotNull
     @Column(nullable = false, name = "started_at")
-    @FutureOrPresent
     private LocalDate startedAt;
 
     @NotNull
@@ -58,12 +51,9 @@ public class Voucher {
     private LocalDate expiredAt;
 
     @Column(nullable = false, updatable = true, name = "quantity")
-    @PositiveOrZero
-    @NotNull
     private Integer quantity;
 
     @Column(nullable = false, name = "is_active")
-    @NotNull
     private Boolean isActive = true;
 
     @AssertTrue(message = "Expired date must be after or equal started date")
