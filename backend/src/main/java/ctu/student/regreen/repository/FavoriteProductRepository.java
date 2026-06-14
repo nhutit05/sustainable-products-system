9f180e41b3c3f52ff48997ad932b106bd32d8b4e
nhutvdev <nhut.vlg2005@gmail.com>
2026-06-14 14:01:26 +0000

    fix: fix id for productmaterial and favoriteproduct repository

--- backend/src/main/java/ctu/student/regreen/repository/FavoriteProductRepository.java
+++ backend/src/main/java/ctu/student/regreen/repository/FavoriteProductRepository.java
@@ -1,12 +1,14 @@
 package ctu.student.regreen.repository;
 
 import ctu.student.regreen.model.FavoriteProduct;
+import ctu.student.regreen.model.FavoriteProductId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface FavoriteProductRepository extends JpaRepository<FavoriteProduct, Integer> {
+public interface FavoriteProductRepository extends JpaRepository<FavoriteProduct, FavoriteProductId> {
 
 
     List<FavoriteProduct> findAllByCustomerUserId(Integer userId);
