c6b968c17ac7721bdca9e0e8d4e858052d639f77
nhutvdev <nhut.vlg2005@gmail.com>
2026-06-18 15:25:03 +0000

    refactor: provide permission for auth

--- backend/src/main/java/ctu/student/regreen/config/SecurityConfig.java
+++ backend/src/main/java/ctu/student/regreen/config/SecurityConfig.java
@@ -43,6 +43,9 @@ public class SecurityConfig {
 
             .authorizeHttpRequests(auth -> auth
 
+                                .requestMatchers("/api/auth/**")
+                                .permitAll()
+                                
                                 .requestMatchers(
                                         "/api/orders/**")
                                 .hasRole(
