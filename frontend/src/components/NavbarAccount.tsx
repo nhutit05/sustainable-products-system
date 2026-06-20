4fdd1ecbfbf34ab48d89c061dcd68adea48cbaf3
hintran_05 <you@example.com>
2026-06-20 10:24:19 +0700

    merge from main

---
+++ frontend/src/components/NavbarAccount.tsx
@@ -0,0 +1,7 @@
+export default function NavbarAccount() {
+  return (
+    <div className="navbar_account">
+      <img src="/avatar.jpg" alt="Avatar" className="navbar_account--avatar" />
+    </div>
+  )
+}
