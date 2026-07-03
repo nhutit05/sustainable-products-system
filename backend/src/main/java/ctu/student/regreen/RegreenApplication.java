4ae3fd880f643efd0bb982d20bc68986e79d8085
nhutvdev <nhut.vlg2005@gmail.com>
2026-07-03 10:06:35 +0000

    feat: add core configuration for payos

--- backend/src/main/java/ctu/student/regreen/RegreenApplication.java
+++ backend/src/main/java/ctu/student/regreen/RegreenApplication.java
@@ -2,8 +2,10 @@ package ctu.student.regreen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class RegreenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegreenApplication.class, args);
